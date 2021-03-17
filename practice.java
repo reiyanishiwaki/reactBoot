a9f62975c02e2c3e90906e399acd79c7157a3f3d
reiya <reiya.nishiwaki.6x@gmail.com>
2021-03-18 01:34:33 +0900

    (add)separate class

---
+++ practice.java
@@ -0,0 +1,9 @@
+class practice {
+    public static void main(String[] args) {
+        Employee reiya = new Employee("reiya", 26, 1124, "SB");
+        System.out.println("name:"+reiya.getName());
+        System.out.println("age:"+reiya.getAge());
+        System.out.println("emp:"+reiya.getComEmployeeNumber());
+        System.out.println("company:"+reiya.getBelongCompanyName());
+    }
+}
